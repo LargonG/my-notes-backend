45ca7702077e25f2a701393a02d94f33deb41300
Anton Panov <pan300503@gmail.com>
2024-03-11 15:21:35 +0300

    Убрал ссылки в scaladoc

--- src/main/scala/org/kote/repository/UpdateCommand.scala
+++ src/main/scala/org/kote/repository/UpdateCommand.scala
@@ -5,13 +5,13 @@ trait UpdateCommand
 object UpdateCommand {
 
   /** Позволяет писать [[UpdatableRepository.update repository.update(obj, cmd)]], вместо того,
-    * чтобы каждый раз добавлять обёртку [[List]]
+    * чтобы каждый раз добавлять обёртку List
     * @param cmd
     *   команда изменения
     * @tparam T
     *   класс изменений конкретного объекта
     * @return
-    *   Оборачивает cmd в [[List]]
+    *   Оборачивает cmd в List
     */
   implicit def commandToList[T <: UpdateCommand](cmd: T): List[T] = List(cmd)
 }
